567f72ed7e7dada3bd16180232d2d6e8e7965f8a
Marlon Gomes <mrlon.mra@gmail.com>
2022-12-14 08:05:29 -0300

    Create FUNCAO-ESCREVE-NOME.js
    Função simples para escrever o nome.

---
+++ FUNCAO-ESCREVE-NOME.js
@@ -0,0 +1,16 @@
+/*FAÇA UMA FUNÇÃO QUE ESCREVA SEU NOME*/
+function EscreverNome(nome){
+
+console.log("Meu nome é:", nome, "!")
+
+};
+
+function main()
+{
+EscreverNome('Marlon');
+EscreverNome('Gomes');
+EscreverNome('Moura');
+};
+
+main();
+
